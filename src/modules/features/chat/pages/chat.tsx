346e53e594d87eeae2c9f1291a27089d742bd82a
GabrielCarvalho03 <gabrielcarvalho1734@gmail.com>
2026-03-06 14:38:15 -0300

    feat: initial commit

---
+++ src/modules/features/chat/pages/chat.tsx
@@ -0,0 +1,17 @@
+export const ChatScreen = () => {
+  return (
+    <div className="w-full bg-white">
+      {/* Hero*/}
+      <main className="h-20 border-b-[0.5px] border-[#F9FAFB]">
+        <section>
+          <div>
+            <h1>Orbita Chat</h1>
+            <div>
+              <p>PLUS</p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
